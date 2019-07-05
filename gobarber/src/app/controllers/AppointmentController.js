dfdd1b84a266992ed292e92f2011867b6787a8a7
henrique <henrique.pedasi@gmail.com>
2019-07-05 15:11:18 -0300

    criada verificaçao de horario usando date fns

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -27,6 +28,38 @@ class AppointmentController {
             return res.status(401).json({
                 error: 'You can only create appointments with providers',
             });
+        }
+
+        /**
+         * CHeck for past dates
+         * Start of Hour pega só o valor da hora e ignora minutos e segundos
+         */
+        const hourStart = startOfHour(parseISO(date));
+        /**
+         * Verifica se a data é anterior a data atual
+         */
+        if (isBefore(hourStart, new Date())) {
+            return res
+                .status(400)
+                .json({ error: 'Past dates are not permited' });
+        }
+
+        /**
+         * Check if is availability
+         */
+
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if (checkAvailability) {
+            return res
+                .status(400)
+                .json({ error: 'Appointment date is not available' });
         }
 
         const appointment = await Appointment.create({
