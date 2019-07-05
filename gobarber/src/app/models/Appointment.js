f13f0899901634466737cd2829fb7e8b853eb58c
henrique <henrique.pedasi@gmail.com>
2019-07-05 12:32:08 -0300

    model de agendamento finalizado

---
+++ gobarber/src/app/models/Appointment.js
@@ -0,0 +1,28 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+    static init(sequelize) {
+        super.init(
+            {
+                date: Sequelize.STRING,
+                cancelet_at: Sequelize.STRING,
+            },
+            {
+                sequelize,
+            }
+        );
+
+        return this;
+    }
+
+    static associate(models) {
+        // Quando o model tem tuas vezes relacionamento com outro, tem que usar um novo nome com 'as'
+        this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+        this.belongsTo(models.User, {
+            foreignKey: 'provider_id',
+            as: 'provider',
+        });
+    }
+}
+
+export default Appointment;
