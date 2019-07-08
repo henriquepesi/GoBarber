93cc9637eaf6bf540d3f24b63ccee69255080c46
henrique <henrique.pedasi@gmail.com>
2019-07-08 16:59:25 -0300

    configuraçao do nodemailer finalizada

---
+++ gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: 'c95b4ef38882b7',
+        pass: 'a3fe408dc00d03',
+    },
+    default: {
+        from: 'Esquipe GoBarber <noreply@gobarber.com.br>',
+    },
+};
